4a3a91a50821f04a8ba76dd94a8bfccdc21ae160
archiebald777 <archiechekatouski777@gmail.com>
2021-12-23 21:15:05 +0200

    fixed file css to scss

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import "./modal.css";
 import beer from "../../assets/images/beer.png"
+import styles from "./styles.module.scss"
+
 const Modal = ({ active, setActive, currentBeer }) => {
   return (
     <div
-      className={active ? "modal active" : "modal"}
+      className={active ? `${styles.modal} ${styles.active}` : styles.modal}
       onClick={() => setActive(false)}
     >
       <div
-        className={active ? "content active" : "content"}
+        className={active ? `${styles.content} ${styles.active}` : styles.content}
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="modalWrapper">
-          <img className="modalImg" src={currentBeer ? currentBeer.image_url || beer : ""} alt="beer Picture" />
-          <div className="modalText">
-          <h1 className="modalName">{currentBeer ? currentBeer.name : ""}</h1>
-          <p className="modalTagLine">{currentBeer ? currentBeer.tagline : ""}</p>
-          <p className="modalAbv">{currentBeer ? currentBeer.abv : ""}</p>
-          <p className="modalDescription">{currentBeer ? currentBeer.description : ""}</p>
-          <p className="modalFoodPairing">{currentBeer ? currentBeer.food_pairing + "" : ""}</p>
+        <div className={styles.modalWrapper}>
+          <img className={styles.modalImg} src={currentBeer ? currentBeer.image_url || beer : ""} alt="beer Picture" />
+          <div className={styles.modalText}>
+          <h1 className={styles.modalName}>{currentBeer ? currentBeer.name : ""}</h1>
+          <p className={styles.modalTagLine}>{currentBeer ? currentBeer.tagline : ""}</p>
+          <p className={styles.modalAbv}>{currentBeer ? currentBeer.abv : ""}</p>
+          <p className={styles.modalDescription}>{currentBeer ? currentBeer.description : ""}</p>
+          <p className={styles.modalFoodPairing}>{currentBeer ? currentBeer.food_pairing + "" : ""}</p>
           </div>
         </div>
       </div>
